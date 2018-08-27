3aebbf0a8aa63769948936e8e4538e150016ac7c
Anton Gilgur <agilgur5@gmail.com>
2018-08-26 23:25:00 -0700

    (feat): add basic Contributors view w/ sample invites
    - just a really basic Carousel view plus an Invite button
      - Carousel will need lots of modifications as it only displays 1
        image at a time, doesn't interact with an API, etc
      - Invites just add a random name + photo for now, will need to
        add a real form once UI is all ready
      - no way to delete for now

--- services/frontEnd/components/app.js
+++ services/frontEnd/components/app.js
@@ -1,8 +1,55 @@
 import React from 'react'
+import { Carousel, Button, Glyphicon, Image } from 'react-bootstrap'
 
-// potentially include React Hot Loader here
-export default class App extends React.Component {
+import styles from './app.cssm'
+
+export default class App extends React.PureComponent {
+  state = {
+    contributors: []
+  }
   render = () => {
-    return <div>Hello World!</div>
+    const { contributors } = this.state
+
+    return <div className={styles.center}>
+      <h2>Contributors</h2>
+      <br />
+      <br />
+      {contributors.length > 0 && <Carousel>
+        {contributors.map(this._renderContributor)}
+      </Carousel>}
+      <br />
+      <br />
+      <Button bsStyle='success' onClick={this._addContributor}>
+        <Glyphicon glyph='plus' /> Invite a Contributor
+      </Button>
+    </div>
+  }
+  _renderContributor = (contributor) => {
+    // photo URIs should be unique
+    return <Carousel.Item key={contributor.photo}>
+      <Contributor contributor={contributor} />
+    </Carousel.Item>
+  }
+
+  _addContributor = () => {
+    const random = Math.random()
+    this.setState(({contributors}) => ({contributors: contributors.concat({
+      name: 'John Doe ' + random,
+      // bust cache on photos
+      photo: 'https://source.unsplash.com/random/100x100?sig=' + random
+    })}))
+  }
+}
+
+class Contributor extends React.PureComponent {
+  render = () => {
+    const { contributor } = this.props
+
+    return <div>
+      <Image circle src={contributor.photo} />
+      <br />
+      <br />
+      <div>{contributor.name}</div>
+    </div>
   }
 }
