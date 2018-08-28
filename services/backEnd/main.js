bd609af850902384640169c13dac4413e185a5b4
Anton Gilgur <agilgur5@gmail.com>
2018-08-27 22:28:10 -0700

    (feat): add simple static asset serving
    - serve the frontEnd service's build folder from the build/ route,
      similar to webpack-serve
      - note that one needs to remember to output the build to disk so
        that there are assets to serve, woops lol
        - webpack-serve by default will only keep the files in-memory
    
    - NOTE: like multiple parts of this challenge for the back-end side,
      I wouldn't recommend using this in production and it's more of an
      overly simplified PoC (which I was told is expected / okay)

--- services/backEnd/main.js
+++ services/backEnd/main.js
@@ -2,4 +2,7 @@ const express = require('express')
 
 const app = express()
 
+// serve static assets from build similarly to webpack-serve config
+app.use('/build', express.static('../frontEnd/build/'))
+
 app.listen(8080, () => console.log('Listening on port 8080'))
