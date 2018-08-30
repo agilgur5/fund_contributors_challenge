1bfc3f095e474d052466ce50f6e19d98d2b47228
Anton Gilgur <agilgur5@gmail.com>
2018-08-29 19:52:15 -0700

    (feat): GET /api/contributors route w/ pagination
    - paginates 6 at a time through the contributors and accepts an offset
      - does some simple validation as well
    
    - added some tests using ava + supertest-fetch as well as nyc for code
      coverage
      - weee 100% coverage! (tho static routes were not tested... those
        are dev only anyway)
    
    - NOTE: similar to the previous commit, for production usage I would
      not recommend holding the contributor list in-memory per process
      like this, this is just an overly simplified PoC (which I was told
      was expected / okay)

--- services/backEnd/main.js
+++ services/backEnd/main.js
@@ -1,8 +1,29 @@
 const express = require('express')
 
+const pageSize = 6
+const contributors = []
 const app = express()
+
+app.get('/api/contributors', (req, res) => {
+  const offset = req.query.offset || 0
+  if (offset > contributors.length || offset < 0) {
+    return res.status(400).json({error: 'Invalid offset'})
+  }
+  return res.json({contributors: contributors.slice(offset, offset + pageSize)})
+})
 
 // serve static assets from build similarly to webpack-serve config
 app.use('/build', express.static('../frontEnd/build/'))
 
-app.listen(8080, () => console.log('Listening on port 8080'))
+// only start the server if run directly from Node, not if required by tests
+/* istanbul ignore if */
+if (require.main === module) {
+  app.listen(8080, () => console.log('Listening on port 8080'))
+}
+
+// exclusively for testing purposes
+module.exports = {
+  app,
+  pageSize,
+  contributors
+}
---
+++ services/backEnd/main.test.js
@@ -0,0 +1,45 @@
+const test = require('ava')
+const { makeFetch } = require('supertest-fetch')
+
+const { app, pageSize, contributors } = require('./main.js')
+const fetch = makeFetch(app)
+
+test('GET /api/contributors', async (assert) => {
+  const fixture = [{name: 'John Doe 0'}, {name: 'John Doe 1'},
+    {name: 'John Doe 2'}, {name: 'John Doe 3'}, {name: 'John Doe 4'},
+    {name: 'John Doe 5'}, {name: 'John Doe 6'}]
+
+  function fetchContributors (offset) {
+    let query = offset ? `?offset=${offset}` : ''
+    return fetch(`/api/contributors${query}`)
+      .expect('Content-Type', /json/)
+  }
+  function getContributors (offset) {
+    return fetchContributors(offset)
+      .expect(200)
+      .then(res => res.json())
+  }
+  function getContributorsFail (offset) {
+    return fetchContributors(offset)
+      .expect(400)
+      .then(res => res.json())
+  }
+
+  let json = await getContributors()
+  assert.deepEqual(json, {contributors: []}, 'should output an empty array')
+
+  contributors.push(...fixture)
+  json = await getContributors()
+  assert.deepEqual(json, {contributors: fixture.slice(0, 0 + pageSize)},
+    'should output the first 6 contributors')
+
+  json = await getContributors(1)
+  assert.deepEqual(json, {contributors: fixture.slice(1, 1 + pageSize)},
+    'should properly offset')
+
+  json = await getContributorsFail(-1)
+  assert.is(json.error, 'Invalid offset', 'should 400 negatives')
+
+  json = await getContributorsFail(fixture.length + 1)
+  assert.is(json.error, 'Invalid offset', 'should 400 when > length')
+})
